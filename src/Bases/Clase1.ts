a472b4b2d53d25f05cac6e97da7917e4bf48408a
Alejandro Mora <alejomora1205@gmail.com>
2026-02-27 12:34:50 -0500

    Clase1

---
+++ src/Bases/Clase1.ts
@@ -0,0 +1,54 @@
+
+//Javascript
+
+// let numero: any = 1;
+// var numero = 1;
+// const numero2 = 2;
+
+//Funciones
+
+export function mostrarNombres(nombre: string): string{
+    
+    return "Hola "+nombre; 
+}
+
+export const mostrarNombreFuncionFlecha = (nombre: string):string => {
+    return "Hola"+nombre
+}
+
+//Interfaces y Clases
+
+export interface PokemonInterface{
+    id: number, //2, 3,14 30423423
+    name: string,
+    age?: number
+}
+
+const Pikachu: PokemonInterface = {
+    id: 1,
+    name: "Pikachu",
+    age: 12
+}
+
+export class PokemonTrainer{
+
+    readonly id: number;
+    readonly name: string;
+    private pokemons: Array<PokemonInterface> = [];
+    constructor(id:number, name:string){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    setPokemon(pokemon: PokemonInterface){
+        this.pokemons.push(pokemon);
+    }
+
+    getPokemons(): PokemonInterface[]{
+        return this.pokemons
+    }
+}
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import './style.css'
+import { setupCounter } from './counter.ts'
+import { PokemonInterface, PokemonTrainer } from './Bases/Clase1.ts'
+
+const div = document.getElementById('app')
+
+const Alejandro: PokemonTrainer = new PokemonTrainer(1,"Alejandro");
+
+Alejandro.setPokemon({
+  id: 2,
+  name: "Charizard",
+  age: 4
+})
+Alejandro.setPokemon({
+  id: 1,
+  name: "Pikachu",
+  age: 3
+})
+Alejandro.setPokemon({
+  id: 3,
+  name: "Bulbasaur",
+  age: 5
+})
+
+const pokemons: PokemonInterface[] = Alejandro.getPokemons();
+
+//Map vs for each
+//Map transforma y devuelve un arreglo con la misma cantidad
+//de elementos mientras que foreach unicamente realiza procedimientos por cada elementos de un arreglo pero este no retorna ningun valor
+
+
+// console.log(pokemons);
+div!.innerHTML=`
+<span><h2>Mis pokemones son:</h2>
+${pokemons.map((pokemon) => {
+  return `<p>${pokemon.name}</p>`
+})}
+</span>`
+
+
+
+
+
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
